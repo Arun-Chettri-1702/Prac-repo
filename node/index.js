541e2276ad2a47da8b0df02e6aa105b9094f03ef
Arun Chettri <arunchettri14@gmail.com>
2026-06-26 13:26:30 +0530

    initial commit

---
+++ node/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import os from "os";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.APP_MESSAGE || "No env set";
+const HOSTNAME = process.env.HOSTNAME || os.hostname();
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello from Simple App (Node)",
+        env: ENV,
+        container: HOSTNAME,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Node Hello listening on ${PORT}`);
+});
